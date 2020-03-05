81f1d0f23ffa3a1261d321c5dd70638246809d43
lcs-JZhang <XZhang@lcs.on.ca>
2020-03-04 19:55:53 -0500

    finished shapes

--- IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
+++ IntroToStructures.playground/Pages/5 - Authoring Structures.xcplaygroundpage/Contents.swift
@@ -32,6 +32,144 @@ import Foundation
  
  */
 // Begin your work here...
+struct Parallelogram {
+    
+
+    var base: Double
+    var height: Double
+    var sidelength: Double
+
+    
+    var area: Double {
+        
+        return base * height
+    }
+    
+    var circumference: Double {
+        
+        return sidelength * 2 + base * 2
+    }
+    
+    var areaDescription: String {
+        
+        return "The area of the parallelogram is " + String(format: "%.1f", area) + " square units."
+    }
+    
+    var circumferenceDescription: String {
+        
+        return "The circumference of the parallelograms is " + String(format: "%.1f", circumference)
+    }
+}
+a
+let shape1 = Parallelogram(base: 10, height: 15, sidelength: 10)
+
+// Area
+shape1.area
+
+// Circumference
+shape1.circumference
+
+//area description
+shape1.areaDescription
+
+// circumference description
+shape1.circumferenceDescription
+
+
+
+
+
+
+
+struct Circle {
+    
+    
+    // MARK: Properties
+    var radius: Double
+    
+    // MARK: Ask For Return
+    var area: Double {
+        return pow(radius, 2) * Double.pi
+    }
+    
+    var circumference: Double {
+        return radius * 2 * Double.pi
+        
+    }
+    
+    
+    var areaDescription: String {
+        
+        return "The area of the circle is " + String(format: "%.1f", area) + " square units."
+    }
+    
+    var circumferenceDescription: String {
+        
+        return "The circumference of the circle is " + String(format: "%.1f", circumference)
+    }
+}
+
+let circle1 = Circle(radius: 10)
+
+// Area of the shape
+circle1.area
+
+// Circumference of the shape
+circle1.circumference
+
+// Describe the area in words
+circle1.areaDescription
+
+// Describe the circumference in words
+circle1.circumferenceDescription
+
+
+
+
+struct Cylinder {
+    
+    // MARK: Properties
+    var radius: Double
+    var height: Double
+    
+    // MARK: Ask For Return
+    var baseArea: Double {
+        
+        return pow(radius, 2) * Double.pi
+    }
+    
+    var lateralSurface: Double {
+        
+        return 2 * Double.pi * radius * height
+    }
+    
+    var totalArea: Double {
+        
+        return baseArea * 2 + lateralSurface
+    }
+    
+    var volume: Double {
+        
+        return baseArea * height
+    }
+    
+    
+    var areaDescription: String {
+        
+        return "The area of the base is " + String(format: "%.1f", baseArea) + " square units." + "\n" + "The area of the lateral surface is " + String(format: "%.1f", lateralSurface) + " square units."
+    }
+    
+    var volumeDescription: String {
+        
+        return "The volume of the cylinder is " + String(format: "%.1f", volume) + " cube units."
+    }
+}
+
+
+struct TriangularPrism {
+    var sideA: Double
+    var sideB: Double
+}
 
 
 
